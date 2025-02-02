c6b89e16b1411430ae12e89725be29f4c3854885
Arman Maurya <armanmarya6@gmail.com>
2025-02-02 13:56:16 +0530

    setting up complete

---
+++ demo/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./App.css";
+// import { Slider } from "../../";
+import { Slider } from "react-photo-slider";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <div>
+        saddkfjlsadfjlj
+        <div>
+          <Slider/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
